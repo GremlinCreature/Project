b4a611dd78d81279518e960049e073061df617fa
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-03-29 12:10:03 +1300

    Version 0.1.1
    Reworked the ticket system so array of tickets is a list instead of a dictionary., added lists for open and closed tickets.

--- ticket_system.py
+++ ticket_system.py
@@ -1,23 +1,30 @@
 ticket_id=0
-tickets={}
+tickets=[]
+open_tickets=[]
+closed_tickets=[]
 #calling ticket ID now for later use
 class Ticket:
-    def __init__(self, staff_id, creator_name, email, description, ticket_id, tickets):
+    def __init__(self, staff_id, creator_name, email, description, ticket_id, tickets, open_tickets, closed_tickets):
         self.staff_id=staff_id
         self.creator_name=creator_name
         self.email=email
         self.description=description
         self.ticket_id=ticket_id
         self.tickets=tickets
+        self.open_tickets=open_tickets
+        self.closed_tickets=closed_tickets
 
     def add_new(self):
         self.ticket_id=len(self.tickets)+1
         #generating ID for the ticket
         response="Not yet provided"
-        ticket_info={"Staff ID": self.staff_id,"Creator Name":self.creator_name, "Contact Email":self.email,"Description":self.description, "Response":response}
+        ticket_info={"Staff ID": self.staff_id,"Creator Name":self.creator_name, "Contact Email":self.email,"Description":self.description,"Status": "Open", "Response":response}
         #creating a dictionary with ticket info
-        self.tickets[self.ticket_id]=ticket_info
-        return self.tickets
+        ticket={self.ticket_id:ticket_info}
+        tickets.append(ticket)
+        open_tickets.append(ticket)
 
-new=Ticket(int(input("Input staff ID: ")), input("Input auithor's name: "), input("Input contact email: "), input("Input problem descrtiption: "),ticket_id, tickets)
-print(new.add_new())
+new=Ticket(int(input("Input staff ID: ")), input("Input auithor's name: "), input("Input contact email: "), input("Input problem descrtiption: "),ticket_id, tickets, open_tickets, closed_tickets)
+new.add_new()
+print(tickets)
+print(open_tickets)
