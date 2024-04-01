360cf88984c89ce7ab2420fed636ed8fb4bf09bf
GremlinCreature <160688129+GremlinCreature@users.noreply.github.com>
2024-04-01 17:04:33 +1300

    Version 0.4
    Added functionality that automatically generates new passwords and closes tickets that have "change" and "password" in their description.

--- ticket_system.py
+++ ticket_system.py
@@ -2,7 +2,7 @@ ticket_id=0
 tickets=[]
 open_tickets=[]
 closed_tickets=[]
-#calling ticket ID now for later use
+#calling ticket ID and all the tickets dictionaries now for later use
 class Ticket:
     def __init__(self, staff_id, creator_name, email, description, ticket_id, tickets, open_tickets, closed_tickets):
         self.staff_id=staff_id
@@ -16,25 +16,75 @@ class Ticket:
 
     def add_new(self):
         self.ticket_id=len(self.tickets)+2001
-        #generating ID for the ticket
+#generating ID for the ticket
         response="Not yet provided"
         ticket_info={"Staff ID": self.staff_id,"Creator Name":self.creator_name, "Contact Email":self.email,"Description":self.description,"Status": "Open", "Response":response}
-        #creating a dictionary with ticket info
+#creating a dictionary with ticket info
+        description_lower_case=self.description.lower()
+#converting all letters in description to lower case
+        words = description_lower_case.split()
+#splitting description into a list of single words
+        print(words)
+        if "password" in words and "change" in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password" in words and "change," in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password," in words and "change" in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password" in words and "change." in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password." in words and "change" in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password" in words and "change?" in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password?" in words and "change" in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password" in words and "change!" in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+        if "password!" in words and "change" in words:
+            print("test")
+            ticket_info["Status"] = "Closed"
+            ticket_info["Response"] = "Your new password is: " + self.staff_id[:2] + self.creator_name[:3]
+#Here i had a choice - i could research "translate" function to remove all punctuation or i could just add some extra variations. I've decided to add more variations.
+#Giving the user new password
         ticket={self.ticket_id:ticket_info}
         tickets.append(ticket)
         open_tickets.append(ticket)
 
     def respond(self):
         search_id = int(input("Input ID of a ticket you want to respond to: "))
+#Inputting ID to search for
         for i in open_tickets:
+            #checks every item in the array
             for key, value in i.items():
+#checks every key and value within integrated dictionary
                 if key == search_id:
+#checks if a key corresponds to the search request
                     value["Response"]=input("Input your response: ")
                     question=input("Do you want to close the ticket ? (y/n): ")
+#checks if responder wants to close the ticket
                     if question=="y":
                         value["Status"]="Closed"
                         closed_tickets.append(i)
                         open_tickets.remove(i)
+                else: print("Incorrect ID. Please, try again.")
+#error in case someone inputs a wrong id
 
     def search_ticket(self):
         search_id = int(input("Input ID of a ticket you want to view: "))
@@ -42,8 +92,14 @@ class Ticket:
             for key, value in i.items():
                 if key == search_id:
                     print(tickets[key-2001])
+#due to how the tickets are put into the list, the easiest way to get info would be requesting to print whatever is on the spot corresponding to requested ID-2001. Ex: ID 2005 within the array would be in the position 2005-2001, so position 4
+
+    def display_statistics(self):
+        print("Total number of tickets: ", len(tickets))
+        print("Total number of solved tickets: ", len(closed_tickets))
+        print("Total number of unsolved tickets: ", len(open_tickets))
 
 
-new=Ticket(int(input("Input staff ID: ")), input("Input author's name: "), input("Input contact email: "), input("Input problem descrtiption: "),ticket_id, tickets, open_tickets, closed_tickets)
+new=Ticket(input("Input staff ID: "), input("Input author's name: "), input("Input contact email: "), input("Input problem descrtiption: "),ticket_id, tickets, open_tickets, closed_tickets)
 new.add_new()
-new.search_ticket()
+print(tickets)
